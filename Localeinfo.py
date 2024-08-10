bf594d4ec969b1c00839ea828669b45f49216102
Abdul Rahaman <166613101+abdulabd22@users.noreply.github.com>
2024-08-10 07:55:51 +0530

    Add files via upload

---
+++ Localeinfo.py
@@ -0,0 +1,259 @@
+url_us = 'https://www.amazon.com/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&ignoreAuthState=1&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1&prevRID=GZQZHQXV6KPBRH3DW9QT&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&ref_=ap_sw_aa&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&disableLoginPrepopulate=1&switch_account=signin&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_de = 'https://www.amazon.de/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.de%2F%3Fref_%3Dnav_ya_signin&prevRID=M7GPGDDG9SMATC9AXMAN&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=deflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=deflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_au = 'https://www.amazon.com.au/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.au%2F%3F_encoding%3DUTF8%26ref_%3Dnav_ya_signin&prevRID=MQVETBZGCKH7W57X6CY9&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=auflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=auflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_br = 'https://www.amazon.com.br/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3Fref_%3Dnav_custrec_signin&prevRID=422R4DCJR6XZ518PEJ0Y&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=brflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=brflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_ca = 'https://www.amazon.ca/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.ca%2F%3Fref_%3Dnav_signin&prevRID=RP4WKBQZK54BZX2NYZSD&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=caflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=caflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_fr = 'https://www.amazon.fr/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.fr%2F%3Fref_%3Dnav_ya_signin&prevRID=B5FE6FMHA6JAVTW5KEBH&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=frflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=frflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_in = 'https://www.amazon.com/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&ignoreAuthState=1&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1&prevRID=GZQZHQXV6KPBRH3DW9QT&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&prepopulatedLoginId=&failedSignInCount=0&ref_=ap_sw_aa&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&disableLoginPrepopulate=1&switch_account=signin&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_it = 'https://www.amazon.it/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.it%2F%3F_encoding%3DUTF8%26ref_%3Dnav_custrec_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=itflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&'
+url_jp = 'https://www.amazon.co.jp/-/en/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.jp%2F%3F_encoding%3DUTF8%26ref_%3Dnav_custrec_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=jpflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&'
+url_mx = 'https://www.amazon.com.mx/ap/register?showRememberMe=true&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.mx%2F%3F_encoding%3DUTF8%26ref_%3Dnav_ya_signin&prevRID=5WS1VBBSZW8D5HH4DGNH&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=mxflex&openid.mode=checkid_setup&prepopulatedLoginId=&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=mxflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+url_es = 'https://www.amazon.es/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.es%2F%3F_encoding%3DUTF8%26ref_%3Dnav_custrec_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=esflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&'
+url_uk = 'https://www.amazon.co.uk/ap/register?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.co.uk%2F%3F_encoding%3DUTF8%26ref_%3Dnav_custrec_newcust&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=gbflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&'
+url_nl = 'https://www.amazon.nl/ap/register?showRememberMe=true&openid.pape.max_auth_age=900&language=en&pageId=nlflex&mobileBrowserWeblabTreatment=C&openid.return_to=https%3A%2F%2Fwww.amazon.nl%2Fgp%2Fyourstore%2Fhome%3Flanguage%3Den%26path%3D%252Fgp%252Fyourstore%252Fhome%26useRedirectOnSuccess%3D1%26signIn%3D1%26action%3Dsign-out%26ref_%3Dnav_AccountFlyout_signout&prevRID=MD3CZDH896MS3T2QG4S0&openid.assoc_handle=nlflex&openid.mode=checkid_setup&desktopBrowserWeblabTreatment=C&prepopulatedLoginId=&failedSignInCount=0&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0'
+cid_url = ''
+expected_subject = ''
+
+########################### Address US
+us_adr_line1 = '47 Debevoise St, Brooklyn'
+us_adr_line2 = 'Brooklyn, NY 11206'
+us_city = 'Brooklyn'
+us_state = 'New york'
+us_Zip = '11206'
+us_ph = '1234567890'
+##############################Address DE
+de_adr_line1 = '46'
+de_adr_line2 = 'Flughafenstrasse'
+de_city = 'Hahnbach'
+de_state = 'Freistaat Bayern'
+de_Zip = '92256'
+de_ph = '1234567890'
+#############################Address Au
+au_adr_line1 = '27'
+au_adr_line2 = 'Augusta St'
+au_city = 'Marble Bar'
+au_state = 'WA'
+au_Zip = '6760'
+au_ph = '1234567890'
+############################Address Br
+br_adr_line1 = '55'
+br_adr_line2 = 'Rua Padre Feijo'
+br_city = 'Paraíso'
+br_state = 'SC'
+br_Zip = '89503021'
+br_ph = '1234567890'
+#############################Address ca
+ca_adr_line1 = '9888 Jasper'
+ca_adr_line2 = 'Ave'
+ca_city = 'Edmonton'
+ca_state = 'AB'
+ca_Zip = 'T5J 5C6'
+ca_ph = '1234567890'
+##########################Address fr
+fr_adr_line1 = '8'
+fr_adr_line2 = 'Boulevard de Bercy'
+fr_city = 'Paris'
+fr_state = 'Paris'
+fr_Zip = '75012'
+fr_ph = '1234567890'
+#########################Address it
+it_adr_line1 = '10'
+it_adr_line2 = 'Bosa Marina'
+it_city = 'Bosa'
+it_state = 'Province of Oristano'
+it_Zip = '7FQM+39'
+it_ph = '1234567890'
+#########################Address jp
+jp_adr_line1 = '7 Chome-3-1 Hongo'
+jp_adr_line2 = 'Bunkyo City, Tokyo, Japan'
+jp_city = 'Bunkyo City'
+jp_state = 'Tokyo'
+jp_Zip = '113_8654'
+jp_ph = '5682573865'
+#######################Address mx
+mx_adr_line1 = '20 Plaza San Jacinto'
+mx_adr_line2 = 'San Ángel'
+mx_city = 'Ciudad de México'
+mx_state = 'CDMX'
+mx_Zip = '01000'
+mx_ph = '1234567890'
+######################Address es
+es_adr_line1 = 'Gran Via de les Corts Catalanes'
+es_adr_line2 = '585, 08007 Barcelona, Spain'
+es_city = 'Barcelona'
+es_state = 'Barcelona'
+es_Zip = '08007'
+es_ph = '5682573865'
+######################Address uk
+uk_adr_line1 = 'Urbis Building'
+uk_adr_line2 = 'Cathedral Gardens,Todd St'
+uk_city = 'Manchester City'
+uk_state = 'Manchester'
+uk_Zip = 'M4 3BG'
+uk_ph = '1234567890'
+#########################Address In
+in_adr_line1 = 'Amazon development centre'
+in_adr_line2 = 'perungudi'
+in_city = 'chennai'
+in_state = 'tamilnadu'
+in_Zip = '600096'
+in_ph = '1234567890'
+#########################Address Nl
+nl_adr_line1 = 'Amazon'
+nl_adr_line2 = 'Mr.Treublaan 7, 1097 DP Amsterdam'
+nl_city = 'Amsterdam'
+nl_state = 'Amsterdam'
+nl_Zip = '1097'
+nl_ph = '1234567890'
+
+
+
+def get_url(Account_Locale):
+    if Account_Locale == 'us':
+        url = url_us
+        cid_url = 'view-source:https://www.amazon.co.uk/?ref_=nav_logo'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = us_adr_line1
+        address2 = us_adr_line2
+        address3 = us_city
+        address4 = us_state
+        address5 = us_Zip
+        address6 = us_ph
+        address_page = 'https://www.amazon.com/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'de':
+        url = url_de
+        expected_subject = 'Verifizieren Sie Ihr neues Amazon-Konto'
+        cid_url = 'https://www.amazon.de/gp/gurupa/test/index.html'
+        address1 = de_adr_line1
+        address2 = de_adr_line2
+        address3 = de_city
+        address4 = de_state
+        address5 = de_Zip
+        address6 = de_ph
+        address_page = 'https://www.amazon.de/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'au':
+        url = url_au
+        cid_url = 'https://www.amazon.com.au/gp/gurupa/test/index.html'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = au_adr_line1
+        address2 = au_adr_line2
+        address3 = au_city
+        address4 = au_state
+        address5 = au_Zip
+        address6 = au_ph
+        address_page = 'https://www.amazon.com.au/hz/mycd/myx#/home/settings/payment'
+
+    elif Account_Locale == 'br':
+        url = url_br
+        cid_url = 'https://www.amazon.com.br/gp/gurupa/test/index.html'
+        expected_subject = 'Verifique sua nova conta da Amazon'
+        address1 = br_adr_line1
+        address2 = br_adr_line2
+        address3 = br_city
+        address4 = br_state
+        address5 = br_Zip
+        address6 = br_ph
+        address_page = 'https://www.amazon.com.br/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'ca':
+        url = url_ca
+        cid_url = 'https://www.amazon.ca/gp/gurupa/test/index.html'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = ca_adr_line1
+        address2 = ca_adr_line2
+        address3 = ca_city
+        address4 = ca_state
+        address5 = ca_Zip
+        address6 = ca_ph
+        address_page = 'https://www.amazon.ca/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'fr':
+        url = url_fr
+        cid_url = 'https://www.amazon.fr/gp/gurupa/test/index.html'
+        expected_subject = 'Vérifiez votre nouveau compte Amazon'
+        address1 = fr_adr_line1
+        address2 = fr_adr_line2
+        address3 = fr_city
+        address4 = fr_state
+        address5 = fr_Zip
+        address6 = fr_ph
+        address_page = 'https://www.amazon.fr/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'in':
+        url = url_in
+        cid_url = 'https://www.amazon.in/gp/gurupa/test/index.html'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = in_adr_line1
+        address2 = in_adr_line2
+        address3 = in_city
+        address4 = in_state
+        address5 = in_Zip
+        address6 = in_ph
+        address_page = 'https://www.amazon.in/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'it':
+        url = url_it
+        cid_url = 'https://www.amazon.it/gp/gurupa/test/index.html'
+        expected_subject = 'Verifica il tuo nuovo account Amazon'
+        address1 = it_adr_line1
+        address2 = it_adr_line2
+        address3 = it_city
+        address4 = it_state
+        address5 = it_Zip
+        address6 = it_ph
+        address_page = 'https://www.amazon.it/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'jp':
+        url = url_jp
+        cid_url = 'https://www.amazon.co.jp/gp/gurupa/test/index.html'
+        expected_subject = '新しいAmazonアカウントを確認します or Verify your new Amazon account'
+        address1 = jp_adr_line1
+        address2 = jp_adr_line2
+        address3 = jp_city
+        address4 = jp_state
+        address5 = jp_Zip
+        address6 = jp_ph
+        address_page = 'https://www.amazon.co.jp/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'mx':
+        url = url_mx
+        cid_url = 'https://www.amazon.com.mx/gp/gurupa/test/index.html'
+        expected_subject = 'Verifica tu nueva cuenta de Amazon'
+        address1 = mx_adr_line1
+        address2 = mx_adr_line2
+        address3 = mx_city
+        address4 = mx_state
+        address5 = mx_Zip
+        address6 = mx_ph
+        address_page = 'https://www.amazon.com.mx/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'es':
+        url = url_es
+        cid_url = 'https://www.amazon.es/gp/gurupa/test/index.html'
+        expected_subject = 'Verifica tu nueva cuenta de Amazon'
+        address1 = es_adr_line1
+        address2 = es_adr_line2
+        address3 = es_city
+        address4 = es_state
+        address5 = es_Zip
+        address6 = es_ph
+        address_page = 'https://www.amazon.es/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'uk':
+        url = url_uk
+        cid_url = 'https://www.amazon.co.uk/gp/gurupa/test/index.html'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = uk_adr_line1
+        address2 = uk_adr_line2
+        address3 = uk_city
+        address4 = uk_state
+        address5 = uk_Zip
+        address6 = uk_ph
+        address_page = 'https://www.amazon.co.uk/hz/mycd/myx#/home/settings/payment'
+    elif Account_Locale == 'nl':
+        url = url_nl
+        cid_url = 'https://www.amazon.nl/gp/gurupa/test/index.html'
+        expected_subject = 'Verify your new Amazon account'
+        address1 = nl_adr_line1
+        address2 = nl_adr_line2
+        address3 = nl_city
+        address4 = nl_state
+        address5 = nl_Zip
+        address6 = nl_ph
+        address_page = 'https://www.amazon.nl/hz/mycd/myx#/home/settings/payment'
+    else:
+        print('Locale not supported')
+
+    return url,expected_subject,cid_url,address1,address2,address3,address4,address5,address6,address_page
